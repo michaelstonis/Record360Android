e41349b9e025d80eeee9294279bc298502dfe3b4
Michael Stonis <michael.stonis@eightbot.com>
2019-03-28 08:51:06 -0500

    record 360 xamarin binding setup

---
+++ Record360Android/Additions/Media.cs
@@ -0,0 +1,12 @@
+﻿using Java.Lang;
+
+namespace Com.Record360.Sdk.Model
+{
+    public partial class Media: Object, IComparable
+    {
+        int IComparable.CompareTo(Object obj)
+        {
+            return CompareTo((Media)obj);
+        }
+    }
+}
---
+++ Record360Demo/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Record360Demo
+{
+    [Activity(Label = "Record360Demo", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+           
+            Com.Record360.Sdk.Record360SDK.
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+        }
+    }
+}
+
